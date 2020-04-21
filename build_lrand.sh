e0cb9d2f181f340f2e19361a8a5ba75632ab257b
Hao Zeng <zenghao1019@qq.com>
2020-04-21 16:24:37 +0800

    run test successfully

--- build_lrand.sh
+++ build_lrand.sh
@@ -1,1 +1,3 @@
-gcc -shared -o lrand.dll mt19937-64.c lrand.c -Ilua-5.3.5/src -Llua-5.3.5/src -llua
+gcc -shared -o lrand.dll lua-5.3.5/src/lua53.dll mt19937-64.c lrand.c -Ilua-5.3.5/src
+
+cp lrand.dll /d/Lua
